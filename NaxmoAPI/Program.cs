e65e817c47d5c6fca5711a672633e7b4c6c9734e
Mikael Öberg <mikael.oberg@creuna.se>
2018-02-11 18:00:42 +0100

    bajs

---
+++ NaxmoAPI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl;
+
+namespace NaxmoAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var url = "https://rest.nexmo.com"
+                .AppendPathSegment("sms")
+                .SetQueryParam(new {
+                 api_key = "my_key",
+                 api_secret = "my_secret"
+                });
+        }
+    }
+}
